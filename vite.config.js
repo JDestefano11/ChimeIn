0fbe40d55a18e8db6055e91e2a50f5a99e1e47c5
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-08-21 12:14:50 -0400

    "Update index.html, package.json, main.jsx, and vite.config.js files"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
   base: '/ChimeIn/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 });
